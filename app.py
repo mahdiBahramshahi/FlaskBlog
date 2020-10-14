4077f39fd4dedf75cb45ff435d0c3321f8d0543f
mahdibahramshahi <bahramshahimahdi92@gmail.com>
2020-10-14 15:38:37 +0330

    execute vedio 15

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from config import Development
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Development)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app , db)
 
 @app.route('/')
 def index():
@@ -10,6 +15,7 @@ def index():
 
 
 from mod_admin import admin
+from mod_users import users
 
 app.register_blueprint(admin)
-
+app.register_blueprint(users)
