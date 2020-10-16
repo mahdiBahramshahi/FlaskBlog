70566113ee32eb5b1fae775cfa3a6733c923ef47
mahdibahramshahi <bahramshahimahdi92@gmail.com>
2020-10-16 20:14:23 +0330

    Initialize admin.login view

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,6 +1,14 @@
+from flask import session
 from . import admin
 
 
 @admin.route('/')
 def index():
     return "Hello from admin Index"
+
+
+@admin.route('/login/')
+def login():
+    session['name'] = 'mahdi'
+    print(session)
+    return "<h1>1<h1/>"
