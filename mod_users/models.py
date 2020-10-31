d476e7ce3de8837e0e2cbf2086aa56bd8b0de987
mahdibahramshahi <bahramshahimahdi92@gmail.com>
2020-10-31 17:27:39 +0330

    added a (is_admin) view function

--- mod_users/models.py
+++ mod_users/models.py
@@ -16,3 +16,7 @@ class User(db.Model):
     def check_password(self,password):
         return check_password_hash(self.password ,password)
 
+    def is_admin(self):
+        return self.role == 1
+
+
