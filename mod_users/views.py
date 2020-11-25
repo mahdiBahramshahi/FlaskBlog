62b7b9505d84dde2c34e270432e799184fd4edf1
mahdibahramshahi <bahramshahimahdi92@gmail.com>
2020-11-25 20:17:54 +0330

    part 23

--- mod_users/views.py
+++ mod_users/views.py
@@ -1,5 +1,6 @@
 from flask import request , render_template , flash 
 from wtforms.validators import Email
+from sqlalchemy.exc import IntegrityError
 
 from app import db 
 
@@ -18,13 +19,19 @@ def register():
             form.password.errors.append(error_msg)
             form.confirm_password.errors.append(error_msg)
             return render_template('users/register.html', form=form)
-    new_user = User()
-    new_user.full_name = form.full_name.data
-    new_user.email = form.email.data
-    new_user.set_password(form.password.data)
-    db.session.add(new_user)
-    db.session.commit()
-    flash('You created your account successfully.' , 'success')
-
+        old_user = User.query.filter(User.email.ilike(form.email.data)).first()
+        if old_user:
+            flash('Email is in use.' , 'error')
+            return render_template('users/register.html', form=form)
+        new_user = User()
+        new_user.full_name = form.full_name.data
+        new_user.email = form.email.data
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You created your account successfully.' , 'success')
+        # except IntegrityError:
+        #     db.session.rollback()
+        #     flash('Email is in use.' , 'error')
     return render_template('users/register.html', form=form)
 
