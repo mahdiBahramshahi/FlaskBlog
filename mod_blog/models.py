d439585ffd5ca1e712c192a20a91457ccc1af2d9
mahdibahramshahi <bahramshahimahdi92@gmail.com>
2020-11-27 10:05:25 +0330

    set categories to categoris

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column , Integer , String , Text
 from app import db
 
 class Category(db.Model):
-    __tablename__= 'categories'
+    __tablename__= 'categoris'
     id = Column(Integer , primary_key=True)
     name = Column(String(128),nullable=False,unique=True)
     description = Column(String(256),nullable=True,unique=False)
